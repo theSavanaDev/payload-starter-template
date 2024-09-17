b04f435465edc5490a99bc561cae2ae4fc0abd38
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-09-17 18:23:36 +0300

    refactor: add additional access control filters

--- src/payload/access/authenticated.ts
+++ src/payload/access/authenticated.ts
@@ -4,9 +4,5 @@ import type { User } from "payload";
 type isAuthenticated = (args: AccessArgs<User>) => boolean;
 
 export const authenticated: isAuthenticated = ({ req: { user } }) => {
-	if (user) {
-		return true;
-	} else {
-		return false;
-	}
+	return Boolean(user);
 };
